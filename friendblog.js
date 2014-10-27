038c23a1095982652a8a4b3e9c6bfb36d74a760f
Henry Hirsch <henry@w3-net.de>
2014-10-27 21:45:12 +0100

    [init] Initial commit.

---
+++ friendblog.js
@@ -0,0 +1,60 @@
+// create an entries collection in the MongoDB db
+Entries = new Meteor.Collection('entries');
+
+if (Meteor.isClient) {
+    Accounts.ui.config({
+        passwordSignupFields: 'USERNAME_AND_EMAIL'
+    });
+    
+    // expose the Entries collection to the entries template (as 'entries')
+    Template.entries.entries = function () {
+        // return all entries sorted by time
+        return Entries.find({}, { sort: { time: -1 }});
+    }
+
+// create events for the 'input' template
+Template.input.events = {
+    // map the event (click) for the control for the given selector
+    'click input#sendMessage': function (event) {
+        var user = Meteor.user();
+        if (!user) {
+            return;
+        }
+ 
+        var message = document.getElementById('message');
+        if (message.value != '') {
+            // add the entry to the list of entries
+            Entries.insert({
+                user: user,
+                message: message.value,
+                time: Date.now()
+            });
+ 
+            document.getElementById('message').value = '';
+            message.value = '';
+        }
+    }
+}
+    
+    // create events for the 'entries' template
+    Template.entries.events = {
+        // map the event (click) for the control for the given selector
+        'click input#clear': function (event) {
+            // make call to a server-side function to do remove the entries,
+            // since you can't just call anything you want from the client
+            Meteor.call('removeAllEntries');
+        }
+    }
+    
+}
+
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        // define some methods on the server
+        return Meteor.methods({
+            removeAllEntries: function () {
+                return Entries.remove({});
+            }
+        });
+    });
+}
